7e55fc1b83ea37b878bbdffa45d480219a9f4467
elaineyeh <10456018@ntub.edu.tw>
2021-02-12 22:12:03 +0800

    feat: move .conf to conf.d

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,18 @@ echo $CIRCLE_PROJECT_REPONAME
 cd $CIRCLE_PROJECT_REPONAME
 pwd
 git pull
-mkdir -p ../docker-compose
 
+mkdir -p ../conf.d
+cd ../$CIRCLE_PROJECT_REPONAME
+pwd
+CONF_FILE=`ls ./*.conf`
+echo $CONF_FILE
+for CONF in $CONF_FILE
+do
+   mv $CONF ~/conf.d
+done
+
+mkdir -p ../docker-compose
 envsubst < docker-compose-template.yml > docker-compose-$CIRCLE_PROJECT_REPONAME.yml
 cp docker-compose-$CIRCLE_PROJECT_REPONAME.yml ../docker-compose
 
